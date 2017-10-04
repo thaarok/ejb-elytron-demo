8b2c3f78f8a3d3f16b72e056f988e81637b7d5ce
Jan Kalina <jkalina@redhat.com>
2017-10-04 11:13:33 +0200

    JBEAP-12279-reproducer

--- ejb1/src/main/java/org/wildfly/tutorial/ejb/ejb1/HelloWorldBean.java
+++ ejb1/src/main/java/org/wildfly/tutorial/ejb/ejb1/HelloWorldBean.java
@@ -1,5 +1,11 @@
 package org.wildfly.tutorial.ejb.ejb1;
+
 import org.wildfly.tutorial.ejb.ejb2.HelloUniverse;
+
+import org.wildfly.security.auth.client.AuthenticationConfiguration;
+import org.wildfly.security.auth.client.AuthenticationContext;
+import org.wildfly.security.auth.client.MatchRule;
+import org.wildfly.security.sasl.SaslMechanismSelector;
 
 import javax.annotation.Resource;
 import javax.annotation.security.PermitAll;
@@ -20,31 +26,53 @@ public class HelloWorldBean implements HelloWorld {
     @Resource
     private SessionContext ejbContext;
 
-    @EJB(lookup = "ejb:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse")
+    //@EJB(lookup = "ejb:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse")
     private HelloUniverse helloUniverse;
+
+    private HelloUniverse helloUniverse2;
 
     public String sayHello() {
 
-        // Alternative 1: working
-        /*
-        try {
-            Properties properties = new Properties();
-            properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory"); // not necessary
-            InitialContext context = new InitialContext(properties);
-            helloUniverse = (HelloUniverse) context.lookup("ejb:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse");
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
-        */
+        String x = "Hello World caller=" + ejbContext.getCallerPrincipal();
 
-        // Alternative 2: does not work for remote EJBs (lookups within the java: namespace only)
-        //helloUniverse = (HelloUniverse) ejbContext.lookup("java:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse");
-        
-        try {
-            return "Hello World caller=" + ejbContext.getCallerPrincipal() + ", " + helloUniverse.sayHi();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        x += AuthenticationContext.empty()
+                .with(MatchRule.ALL, AuthenticationConfiguration.empty()
+                        .setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("DIGEST-MD5"))
+                        .useName("user2")
+                        .usePassword("user2a")
+                ).runAction(() -> {
+                    try {
+                        Properties properties = new Properties();
+                        properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+                        //properties.put(Context.PROVIDER_URL, "remote+http://localhost:8180");
+                        InitialContext context = new InitialContext(properties);
+                        helloUniverse = (HelloUniverse) context.lookup("ejb:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse");
+                        return ", 1:" + helloUniverse.sayHi();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return ", 1:" + e.toString();
+                    }
+                });
+
+        x += AuthenticationContext.empty()
+                .with(MatchRule.ALL, AuthenticationConfiguration.empty()
+                        .setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("DIGEST-MD5"))
+                        .useName("user2")
+                        .usePassword("user2b")
+                ).runAction(() -> {
+                    try {
+                        Properties properties = new Properties();
+                        properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+                        InitialContext context = new InitialContext(properties);
+                        helloUniverse2 = (HelloUniverse) context.lookup("ejb:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse");
+                        return ", 2:" + helloUniverse2.sayHi();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return ", 2:" + e.toString();
+                    }
+                });
+
+        return x;
     }
 
 }

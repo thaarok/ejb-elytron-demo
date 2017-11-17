af42fa2e04d36c0ca8d99c7909a173c9e7dc92fe
Jan Kalina <jkalina@redhat.com>
2017-11-17 23:44:06 +0100

    JBEAP-12894 reproducer

--- client/src/main/java/org/wildfly/tutorial/ejb/client/Client.java
+++ client/src/main/java/org/wildfly/tutorial/ejb/client/Client.java
@@ -4,12 +4,18 @@ import org.wildfly.security.auth.client.AuthenticationConfiguration;
 import org.wildfly.security.auth.client.AuthenticationContext;
 import org.wildfly.security.auth.client.MatchRule;
 import org.wildfly.security.sasl.SaslMechanismSelector;
-import org.wildfly.tutorial.ejb.ejb1.HelloWorld;
+import org.wildfly.security.ssl.SSLConfigurator;
+import org.wildfly.security.ssl.SSLContextBuilder;
 import org.wildfly.tutorial.ejb.ejb2.HelloUniverse;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
+import java.io.FileInputStream;
+import java.security.KeyStore;
 import java.util.Properties;
 
 public class Client {
@@ -17,15 +23,16 @@ public class Client {
 
         AuthenticationContext.empty()
                 .with(MatchRule.ALL, AuthenticationConfiguration.empty()
-                        .setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("DIGEST-MD5"))
-                        .useName("user2")
-                        .usePassword("user2")
+                        //.setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("SCRAM-SHA-1-PLUS"))
+                        .setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("SCRAM-SHA-1"))
+                        .useName("user")
+                        .usePassword("user")
                 ).run(() -> {
             try {
                 Properties properties = new Properties();
                 properties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
                 properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-                properties.put(Context.PROVIDER_URL, "http-remoting://localhost:8180");
+                properties.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
                 Context context = new InitialContext(properties);
 
                 HelloUniverse helloUniverse = (HelloUniverse) context.lookup("ejb:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse");
@@ -36,21 +43,31 @@ public class Client {
             }
         });
 
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream("/home/jkalina/work/wildfly/build/target/wildfly-12.0.0.Alpha1-SNAPSHOT/standalone/configuration/keystore.jks"), "secret1".toCharArray());
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(ks);
+
         AuthenticationContext.empty()
                 .with(MatchRule.ALL, AuthenticationConfiguration.empty()
-                        .setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("DIGEST-MD5"))
-                        .useName("user1")
-                        .usePassword("user1")
+                        //.setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("SCRAM-SHA-1-PLUS"))
+                        .setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("SCRAM-SHA-1"))
+                        .useName("user")
+                        .usePassword("user")
+                ).withSsl(MatchRule.ALL, new SSLContextBuilder()
+                        .setTrustManager((X509TrustManager) tmf.getTrustManagers()[0])
+                        .setClientMode(true)
+                        .build()
                 ).run(() -> {
             try {
                 Properties properties = new Properties();
                 properties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
                 properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-                properties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+                properties.put(Context.PROVIDER_URL, "remote+https://localhost:8443");
                 Context context = new InitialContext(properties);
 
-                HelloWorld bean = (HelloWorld) context.lookup("ejb:ejb1ear-1.0-SNAPSHOT/ejb1/HelloWorldBean!org.wildfly.tutorial.ejb.ejb1.HelloWorld");
-                System.out.println(bean.sayHello());
+                HelloUniverse helloUniverse = (HelloUniverse) context.lookup("ejb:/ejb2-1.0-SNAPSHOT/HelloUniverseBean!org.wildfly.tutorial.ejb.ejb2.HelloUniverse");
+                System.out.println(helloUniverse.sayHi());
 
             } catch (Exception e) {
                 e.printStackTrace();
